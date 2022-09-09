c90e4ff785792d0c5b16e8c76a7bfb90f600506b
Ben Young <ben.young@ramseysolutions.com>
2022-09-09 15:15:37 -0500

    added dropdown menu

--- pages/login.js
+++ pages/login.js
@@ -3,7 +3,7 @@ import React, { useEffect } from "react";
 import Layout from "../components/layout";
 import { signIn, useSession } from "next-auth/react";
 import { useForm } from "react-hook-form";
-import { getError } from "../utils/error";
+import { getError } from "../utils/errors";
 import { toast } from "react-toastify";
 import { useRouter } from "next/router";
 
